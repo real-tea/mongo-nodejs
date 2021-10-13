ecce1d276298e81a1e7db20ce7638ab67348f07b
Akash <akash.0711a@gmail.com>
2021-10-13 13:21:03 +0530

    new files

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
  
 const newModel = new mongoose.Schema({
     record : {type : String,require : true},
-    date : {type : Number, require : true, default : 100}
+    date : {type : Number, require : true, default : Date.now()}
 })
 
 const model = mongoose.model('TodoModel',newModel);
--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const bodyParser = require('body-parser');
 const Todo = require('./models/todo');
 const app = express();
 
+
+mongoose.connect('mongodb://localhost:27017/firstDatabase')
+
 app.use(bodyParser.json());
 
-console.log('Todo is =>',Todo);
-console.log('Mongoose model is : ',mongoose.model('TodoModel'))
+// console.log('Todo is =>',Todo);
+// console.log('Mongoose model is : ',mongoose.model('TodoModel'))
 
 app.use('/',express.static(path.join(__dirname, 'assets')))
+
+app.post('/',async (req,res)=>{
+    const record = req.body
+    console.log(record)
+
+    const response = await Todo.create(record)
+    console.log(response);
+    res.json({ status : "ok" })
+
+})
 
 app.listen(3000,()=>{
     console.log("server up :)");
